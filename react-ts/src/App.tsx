ec69353d447428ed6286123672514f9329cb62d5
Jeremy Perras <jperras@k2r3p5.42mulhouse.fr>
2022-09-26 14:04:45 +0200

    Start

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,13 @@
 import About from './pages/About';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home'; 
 import Game from './pages/Game';
 const App = () => {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/game" element={<Game />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<About />} />
         </Routes>
       </BrowserRouter>
     );
