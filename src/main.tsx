40d5bf61eb838bde5768697a523d3acee6e83a57
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-10-07 13:42:19 +0200

    save

--- src/main.tsx
+++ src/main.tsx
@@ -1,75 +1,78 @@
-import React from "react";
-import { useState } from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom/client";
 import { Transition } from "@headlessui/react";
+import { useMediaQuery } from "@react-hookz/web/esm";
 import Icon from "@mdi/react";
 import { mdiAccountGroup, mdiClose } from "@mdi/js";
-
-import Dialog from "./Dialog";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import "./index.css";
 
-function Social({ className }: { className?: string }) {
-  const [isShowing, setIsShowing] = useState(true);
-  const [showSocialBtn, setShowSocialBtn] = useState(false);
+function SocialBar() {
+  return <div className="basis-128 flex flex-col bg-stone-50"></div>;
+}
+
+function SocialModal() {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="flex overflow-hidden">
-      {showSocialBtn && (
-        <span className=" delay-700 relative inline-flex self-start">
-          <button
-            type="button"
-            className="inline-flex  px-4 py-2"
-            onClick={() => {
-              setIsShowing(() => true);
-              setShowSocialBtn(() => false);
-            }}
-          >
-            <Icon
-              path={mdiAccountGroup}
-              title="Social"
-              size={1.5}
-              className={isShowing ? "text-slate-800" : "text-slate-300"}
-            />
-          </button>
-          <span className="absolute top-3 right-3 -mt-1 -mr-1 flex h-3 w-3">
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      {!open && (
+        <Dialog.Trigger className="absolute top-2 right-4  inline-flex animate-[fadeIn_700ms_ease_300ms_1_normal_backwards] ">
+          <Icon
+            className="inline-flex"
+            path={mdiAccountGroup}
+            title="Social"
+            size={1.5}
+          />
+          <span className="absolute top-0 right-0 -m-1 flex h-3 w-3">
             <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-red-400 opacity-75"></span>
             <span className="relative inline-flex h-3 w-3 rounded-full bg-red-500"></span>
           </span>
-        </span>
+        </Dialog.Trigger>
       )}
       <Transition
-        show={isShowing}
-        enter="transform transition ease-in-out duration-500"
+        as={Fragment}
+        show={open}
+        enter=" transition-translate ease-in-out duration-200"
         enterFrom="translate-x-full"
         enterTo="translate-x-0"
-        leave="transform transition ease-in-out duration-500"
+        leave=" transition-translate ease-in-out duration-200"
         leaveFrom="translate-x-0"
         leaveTo="translate-x-full"
-        afterLeave={() => setShowSocialBtn(() => true)}
       >
-        <div
-          className={`${className} top-0 right-0 h-full w-96 grow-0 bg-slate-300 lg:static lg:block lg:basis-96`}
+        <Dialog.Content
+          forceMount
+          className="w-128 absolute right-0 flex h-full flex-col bg-slate-50"
         >
-          <button
-            type="button"
-            className="inline-flex  px-4 py-2"
-            onClick={() => setIsShowing(() => false)}
-          >
-            <Icon path={mdiClose} title="close" size={1} />
-          </button>
-        </div>
+          <Dialog.Close className="mx-4 my-2 self-end">
+            <Icon path={mdiClose} size={1} className="text-slate-400" />
+          </Dialog.Close>
+          <Dialog.Title>Edit profile</Dialog.Title>
+          <Dialog.Description>
+            Make changes to your profile here. Click save when youre done.
+          </Dialog.Description>
+        </Dialog.Content>
       </Transition>
+    </Dialog.Root>
+  );
+}
+
+function App() {
+  const smallScreen = useMediaQuery("(min-width: 1536px)");
+
+  return (
+    <div className="relative flex h-full overflow-hidden bg-black text-slate-50">
+      <div
+        className="grow basis-auto bg-cover bg-center bg-no-repeat"
+        style={{ "background-image": "url(pong.webp)" }}
+      ></div>
+      {smallScreen ? <SocialBar /> : <SocialModal />}
     </div>
   );
 }
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="flex h-full bg-slate-50">
-      <div className="flex grow basis-auto flex-col  "></div>
-      <Dialog />
-      <Social />
-    </div>
+    <App />
   </React.StrictMode>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+    },
   },
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   plugins: [require("tailwindcss-radix")()],
