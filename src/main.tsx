5587d4ecb2cd84a76f34025cc48c188242e7c14b
Jeremy Perras <jperras@k2r4p6.42mulhouse.fr>
2022-10-10 11:44:49 +0200

    Mouse on enter

--- src/main.tsx
+++ src/main.tsx
@@ -24,16 +24,41 @@ const Open_back = ({
   );
 };
 
-const Rules = ({ className }: { className: string }) => {
-  return <div className={`${className}  bg-black`}></div>;
+const Rules = ({
+  className,
+  state,
+  setState,
+}: {
+  className: string;
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const isEnter = () => {
+    setState(!state);
+  };
+
+  return (
+    <div
+      className={`${className} h-full w-full bg-black`}
+      onMouseEnter={isEnter}
+    ></div>
+  );
 };
+
 const First_Card = ({
   isOpen,
   setIsOpen,
+  state,
+  setState,
 }: {
   isOpen: boolean;
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const isEnter = () => {
+    setState(!state);
+  };
   return (
     <div
       style={{
@@ -43,9 +68,13 @@ const First_Card = ({
         height: "70%",
         backgroundSize: "40%",
       }}
-      className="flex h-full w-full items-center justify-center"
+      className={` ${
+        state ? " " : "bg-black"
+      } flex h-full w-full items-center justify-center`}
+      onMouseEnter={isEnter}
+      onMouseLeave={isEnter}
     >
-      <Rules className="w-full h-full bg-black" />
+      {/* <Rules className="" state={state} setState={setState} /> */}
       <motion.div
         className="m-2 h-full w-full rounded "
         initial={{ opacity: 0.6 }}
@@ -58,15 +87,117 @@ const First_Card = ({
   );
 };
 
+const Second_Card = ({
+  isOpen,
+  setIsOpen,
+  state,
+  setState,
+}: {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const isEnter = () => {
+    setState(!state);
+  };
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${Verso})`, //change bg if is open or not
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "70%",
+        backgroundSize: "40%",
+      }}
+      className={` ${
+        state ? " " : "bg-black"
+      } flex h-full w-full items-center justify-center`}
+      onMouseEnter={isEnter}
+      onMouseLeave={isEnter}
+    >
+      {/* <Rules className="" state={state} setState={setState} /> */}
+      <motion.div
+        className="m-2 h-full w-full rounded "
+        initial={{ opacity: 0.6 }}
+        whileHover={{ scale: [null, 1.3, 1.2], opacity: 1 }}
+        whileTap={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        onClick={() => setIsOpen((isOpen) => !isOpen)}
+      />
+    </div>
+  );
+};
+
+const Third_Card = ({
+  isOpen,
+  setIsOpen,
+  state,
+  setState,
+}: {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const isEnter = () => {
+    setState(!state);
+  };
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${Verso})`, //change bg if is open or not
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        height: "70%",
+        backgroundSize: "40%",
+      }}
+      className={` ${
+        state ? " " : "bg-blue-600"
+      } flex h-full w-full items-center justify-center`}
+      onMouseEnter={isEnter}
+      onMouseLeave={isEnter}
+    >
+      {/* <Rules className="" state={state} setState={setState} /> */}
+      <motion.div
+        className="m-2 h-full w-full rounded bg-slate-700"
+        initial={{ opacity: 0.6 }}
+        whileHover={{ scale: [null, 1.3, 1.2], opacity: 1 }}
+        whileTap={{ scale: 1.1 }}
+        transition={{ duration: 0.3 }}
+        onClick={() => setIsOpen((isOpen) => !isOpen)}
+      />
+    </div>
+  );
+};
 const App = () => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const [stateFirst, setStateFirst] = React.useState(true);
+  const [stateSecond, setStateSecond] = React.useState(true);
+  const [stateThird, setStateThird] = React.useState(true);
   return (
     <>
       <div className="flex h-full w-full flex-row items-center justify-center bg-slate-200 p-2">
-        <First_Card isOpen={isOpen} setIsOpen={setIsOpen} />
-        <First_Card isOpen={isOpen} setIsOpen={setIsOpen} />
-        <First_Card isOpen={isOpen} setIsOpen={setIsOpen} />
+        <First_Card
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          state={stateFirst}
+          setState={setStateFirst}
+        />
+        <Second_Card
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          state={stateSecond}
+          setState={setStateSecond}
+        />
+        <Third_Card
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          state={stateThird}
+          setState={setStateThird}
+        />
       </div>
+
       {isOpen ? <Open_back isOpen={isOpen} setIsOpen={setIsOpen} /> : ""}
     </>
   );
