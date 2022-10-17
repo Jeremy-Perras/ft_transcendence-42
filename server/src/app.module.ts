a94eb05edeb35e1a98ce1c484a193238d064e42a
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-10-17 15:01:58 +0200

    Basic CRUD generation

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,21 @@
 import { Module } from "@nestjs/common";
+import { MessagesModule } from "./api/messages/messages.module";
+import { ChannelsModule } from "./api/channels/channels.module";
+import { UsersModule } from "./api/users/users.module";
+import { GamesModule } from "./api/games/games.module";
 import { join } from "path";
 import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "../../../", "client/dist"),
       exclude: ["/api/*"],
     }),
+    MessagesModule,
+    ChannelsModule,
+    UsersModule,
+    GamesModule,
   ],
 })
 export class AppModule {}
