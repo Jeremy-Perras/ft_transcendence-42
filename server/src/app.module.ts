0dddc250ce98b1e213f49b88db84b0f401b75ef1
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-10-17 15:01:03 +0200

    Nestjs cleanup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
   imports: [],
