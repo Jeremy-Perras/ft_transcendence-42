d9dc9e877d144993dd0d10ef1eef164552204df4
Jenny Eulliot <jeulliot@k2r4p7.42mulhouse.fr>
2022-10-20 14:37:50 +0200

    fetch hook

--- client/src/sidebar/pages/channel.tsx
+++ client/src/sidebar/pages/channel.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+async function useGetChannel(url: string): Promise<string> {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    let ignore = false;
+    fetch(new URL(url))
+      .then((response) => response.json())
+      .then((json) => {
+        if (!ignore) {
+          console.log(json);
+        }
+      });
+    return () => {
+      ignore = true;
+    };
+  }, [url]);
+
+  return "";
+}
+
+// const res = await fetch(url);
+// const json = await res.json();
+
 export default function Channel() {
+  const [test, setTest] = useState("");
+  const url = "http://localhost:3000/api/channels/v";
+  const results = useGetChannel(url);
   return (
     <>
+      <input type="text" onChange={(e) => setTest(e.target.value)} />
       <h1 className="text-lg">channel</h1>
       <ul>
         <li>
@@ -18,6 +45,7 @@ export default function Channel() {
           <Link to="/profile/user">profile</Link>
         </li>
       </ul>
+      <div className="text-white"></div>
     </>
   );
 }
