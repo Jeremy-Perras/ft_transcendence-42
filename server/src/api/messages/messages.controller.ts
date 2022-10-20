5f63e21d9424d3b588545e576b495f82779798d7
Jenny Eulliot <jeulliot@k2r4p7.42mulhouse.fr>
2022-10-20 11:18:27 +0200

    api gets

--- server/src/api/messages/messages.controller.ts
+++ server/src/api/messages/messages.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param, Query } from "@nestjs/common";
 import { MessagesService } from "./messages.service";
 
 @Controller("api/messages")
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
+  @Get()
+  findDirectMessages(@Query("u") query: number) {
+    if (query) {
+      return this.messagesService.findDirectMessages(query);
+    }
+  }
+
+  @Get()
+  findChannelMessages(@Query("c") query: string) {
+    if (query) {
+      return this.messagesService.findChannelMessages(query);
+    }
+  }
 }
--- server/src/api/messages/messages.service.ts
+++ server/src/api/messages/messages.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { generateMock } from "@anatine/zod-mock";
+import { MessageSchema } from "shared";
+import { Userschema } from "../users/users.dto";
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  findDirectMessages(query: number) {
+    return generateMock(MessageSchema);
+  }
+
+  findChannelMessages(query: string) {
+    return generateMock(MessageSchema);
+  }
+}
