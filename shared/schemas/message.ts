2ee7086dbecae592908e0583c3590e2429629e2f
Jeremy Perras <jperras@k2r4p6.42mulhouse.fr>
2022-10-21 16:22:13 +0200

    design channel

--- shared/schemas/message.ts
+++ shared/schemas/message.ts
@@ -5,10 +5,8 @@ export const MessageSchema = z.object({
   author: UserSchema,
   content: z.string().min(1),
   sentAt: z.preprocess((val) => {
-    if (typeof val === "string")
-      return new Date(val)
-    if (val instanceof Date)
-      return val
-    return false
-  }, z.date())
+    if (typeof val === "string") return new Date(val);
+    if (val instanceof Date) return val;
+    return false;
+  }, z.date()),
 });
