41159a008409bf63daeb50d199948e4fbed9cc85
Jeremy Perras <jperras@k2r4p6.42mulhouse.fr>
2022-10-26 16:23:14 +0200

    Start graphql

--- apps/server/src/channel/channel.resolver.ts
+++ apps/server/src/channel/channel.resolver.ts
@@ -1,8 +1,45 @@
-import { Resolver } from "@nestjs/graphql";
+import {
+  Args,
+  Int,
+  Query,
+  ResolveField,
+  Resolver,
+  Root,
+} from "@nestjs/graphql";
+import { channel } from "diagnostics_channel";
 import { PrismaService } from "../prisma/prisma.service";
+import { User } from "../user/user.model";
 import { Channel } from "./channel.model";
 
 @Resolver(Channel)
 export class ChannelResolver {
   constructor(private prisma: PrismaService) {}
+  @Query((returns) => [Channel], { nullable: true })
+  findChannel(@Args("name", { nullable: true }) name?: string) {
+    return this.prisma.channel.findMany({
+      include: { owner: true },
+      where: {
+        name: {
+          contains: name ?? undefined,
+        },
+      },
+    });
+  }
+
+  @Query((returns) => Channel, { nullable: true })
+  FindChannelById(@Args("id", { type: () => Int }) id: number) {
+    return this.prisma.user.findUnique({ where: { id: id } });
+  }
+
+  @Query((returns) => [Channel], { nullable: true })
+  findChannelByOwner(@Args("name", { nullable: true }) name?: string) {
+    return this.prisma.channel.findMany({
+      include: { owner: true },
+      where: {
+        owner: {
+          name: { contains: name ?? undefined },
+        },
+      },
+    });
+  }
 }
