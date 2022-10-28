9b56f3669e4147331f820c0eb53e20d73bb7b471
Jenny Eulliot <jeulliot@k0r3p13.42mulhouse.fr>
2022-10-28 12:27:13 +0200

    fixed param issue

--- apps/client/src/sidebar/pages/channel.tsx
+++ apps/client/src/sidebar/pages/channel.tsx
@@ -1,48 +1,45 @@
 import { Link, useParams } from "react-router-dom";
 import { useGetChannelQuery } from "../../graphql/generated";
 
-// const displayChannelMessages = (
-//   messages: z.infer<typeof ChannelSchema.shape.messages>
-// ) => {
-//   return (
-//     <>
-//       {[...messages].map((message: any, index: number) => {
-//         return (
-//           <div key={index}>
-//             <div className="mt-5 ml-2 mr-2 flex w-auto flex-col border-2 bg-slate-100">
-//               <div className="text-end text-sm">{`${message.content}`}</div>
-//             </div>
-//             <div className="ml-2 mr-2 w-auto align-text-bottom text-xs text-slate-500">{`Sent by ${message.author.name} at ${message.sentAt}`}</div>
-//           </div>
-//         );
-//       })}
-//     </>
-//   );
-// };
+const displayChannelMessages = (messages: any) => {
+  console.log(messages);
 
-// displays one channel with messages
+  return (
+    <>
+      {/* {{[...messages].map((message: any, index: number) => {
+        return (
+          <div key={index}>
+            <div className="mt-5 ml-2 mr-2 flex w-auto flex-col border-2 bg-slate-100">
+              <div className="text-end text-sm">{`${message.content}`}</div>
+            </div>
+            <div className="ml-2 mr-2 w-auto align-text-bottom text-xs text-slate-500">{`Sent by ${message.author.name} at ${message.sentAt}`}</div>
+          </div>
+        );
+      })} */}
+    </>
+  );
+};
+
 const UniqueChannelQuery = () => {
   const params = useParams();
-  //TODO: get id as int from params
 
-  // const { isLoading, isFetching, error, data } = useGetChannelQuery(
-  //   params?.channelId,
-  //   {}
-  // );
-  // console.log(data);
+  const { isLoading, isFetching, error, data } = useGetChannelQuery({
+    channelId: +params.channelId,
+  });
 
   return (
     <div className="mb-2 mt-2 flex w-full flex-col border-t-2 border-slate-600 p-2">
       <div className="w-full flex-col items-center justify-center border-2 border-black p-2 text-center text-sm">
         <div>Channel id: {params.channelId}</div>
-        {/* <div>Channel: {channel.name}</div>
-          <div>Owner: {channel.owner.name}</div> */}
+        <div>Channel: {data?.channel.name}</div>
+        <div>Owner: {data?.channel.owner.name}</div>
       </div>
       <div className="h-full w-full place-items-center">
-        {/* {displayChannelMessages(channel.messages)} */}
+        {displayChannelMessages(data?.channel.messages)}
       </div>
     </div>
   );
+
   // }
 };
 
