c69a461ac722ea5be54417071f9911a2239f1e6a
Jeremy Perras <jperras@k2r4p6.42mulhouse.fr>
2022-10-31 09:11:03 +0100

    Start chat

--- apps/client/src/sidebar/pages/chat.tsx
+++ apps/client/src/sidebar/pages/chat.tsx
@@ -1,18 +1,40 @@
 import { Link, useParams } from "react-router-dom";
+import { useDirectMessagesQuery } from "../../graphql/generated";
 
 const DirectConversation = () => {
   const params = useParams();
-
-  return (
-    <div className="mb-2 mt-2 flex w-full flex-col border-t-2 border-slate-600">
-      <div className="p-2 text-center">
-        Conversation with user {params?.userId}
+  const { isLoading, data, error, isFetching } = useDirectMessagesQuery({
+    userId: +params.userId,
+  });
+  console.log(data);
+  if (isLoading) return <div>Loading ...</div>;
+  if (isFetching) {
+    return <div>Fetching</div>;
+  }
+  if (error) {
+    return <div>Error</div>;
+  } else {
+    return (
+      <div className="mb-2 mt-2 flex w-full flex-col border-t-2 border-slate-600">
+        <div className="p-2 text-center">
+          Conversation with user {params?.userId}
+        </div>
+        {data?.user.messages.map((message, index) => (
+          <div
+            key={index}
+            className={`${
+              message.author.id === +params.userId
+                ? "bg-red-500 text-right"
+                : "bg-blue-300 text-left"
+            }`}
+          >
+            {message.content}
+          </div>
+        ))}
       </div>
-    </div>
-  );
-  // }
+    );
+  }
 };
-
 export default function Chat() {
   return (
     <div>
