505d724b21768fff6a95d3645102362423aa7cfd
Ptitjen <97309340+Ptitjen@users.noreply.github.com>
2022-11-01 11:06:25 +0100

    save

--- apps/client/src/sidebar/pages/home.tsx
+++ apps/client/src/sidebar/pages/home.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { useNavigate } from "react-router-dom";
 import * as Avatar from "@radix-ui/react-avatar";
 import { ReactComponent as UserIcon } from "pixelarticons/svg/user.svg";
