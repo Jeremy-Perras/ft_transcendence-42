794257945ea2b0511b86620c096d7d13a5757e6c
Jenny Eulliot <jeulliot@k1r2p1.42mulhouse.fr>
2022-11-10 14:59:48 +0100

    add shadow to slider

--- apps/client/src/sidebar/pages/profile.tsx
+++ apps/client/src/sidebar/pages/profile.tsx
@@ -13,8 +13,26 @@ import {
   useUpdateFriendMutation,
   useUpdateUnFriendMutation,
 } from "../../graphql/generated";
+import Rank1Icon from "/src/assets/images/Rank1.svg";
+import Rank2Icon from "/src/assets/images/Rank2.svg";
+import Rank3Icon from "/src/assets/images/Rank3.svg";
+import Rank4Icon from "/src/assets/images/Rank4.svg";
+import Rank5Icon from "/src/assets/images/Rank5.svg";
+import { ReactComponent as UserIcon } from "pixelarticons/svg/user.svg";
+import { ReactComponent as AddFriendIcon } from "pixelarticons/svg/user.svg";
 import { ReactComponent as PlayIcon } from "pixelarticons/svg/gamepad.svg";
 
+export const RankIcon = (rank: number) => {
+  return rank <= 10
+    ? Rank1Icon
+    : rank <= 20
+    ? Rank2Icon
+    : rank <= 30
+    ? Rank3Icon
+    : rank <= 40
+    ? Rank4Icon
+    : Rank5Icon;
+};
 const query = (
   userId: number
 ): UseQueryOptions<UserProfileQuery, unknown, UserProfileQuery> => {
@@ -33,7 +51,7 @@ export const loader =
     }
   };
 
-const Friend = ({
+const DisplayAddAsFriend = ({
   data,
   userId,
 }: {
@@ -48,29 +66,39 @@ const Friend = ({
   });
 
   return (
-    <div className="flex w-full flex-col ">
-      <div className="flex flex-col items-center justify-center shadow-md">
-        <div className="flex w-full flex-col items-center justify-center">
+    <div className="flex w-full flex-col  justify-center ">
+      <div className="flex items-center justify-start ">
+        {typeof data?.user.avatar !== undefined && data?.user.avatar !== "" ? (
           <img
             src={data?.user.avatar}
             alt="Player avatar"
-            className="my-4 h-24 w-24 "
+            className="my-2 ml-2 mr-4 h-28 w-28 border border-black"
           />
-          <div className="mb-4 flex flex-col text-center">
-            <div className="text-xl font-bold">{data?.user.name}</div>
-            <div>Rank : {data?.user.rank}</div>
-            <div>Statistics : TO DO</div>
-          </div>
+        ) : (
+          <UserIcon className="my-2 ml-2 mr-4 h-28 w-28 border border-black text-neutral-700" />
+        )}
+        <img
+          src={RankIcon(data?.user.rank)}
+          className="-translate-x-8 -translate-y-8"
+        />
+        <div className="my-4 flex flex-col text-left">
+          <div className="text-xl font-bold">{data?.user.name}</div>
+
+          <div>Statistics : TODO </div>
         </div>
-        <span
-          onClick={() => {
-            askFriend.mutate({ updateFriendId: userId });
-          }}
-          className="my-12 flex h-36 w-36 items-center justify-center self-center border-2 border-slate-300 bg-slate-200 p-2 text-center text-2xl  font-bold transition-all  hover:cursor-pointer hover:bg-slate-300"
-        >
-          Add as Friend
-        </span>
       </div>
+      <div className="mt-24 w-48 self-center text-center text-xl text-slate-300">
+        {data?.user.name} is not your friend !
+      </div>
+      <span
+        onClick={() => {
+          askFriend.mutate({ updateFriendId: userId });
+        }}
+        className="my-8 flex h-24 w-24 items-center justify-center self-center border-2 border-slate-300 bg-slate-200 p-2 text-center text-xl  font-bold transition-all  hover:cursor-pointer hover:bg-slate-300"
+      >
+        Add as Friend
+      </span>
+      <AddFriendIcon className="w-96 self-center text-slate-100 " />
     </div>
   );
 };
@@ -102,15 +130,23 @@ const DisplayUserProfile = ({
 
   return (
     <div className="flex h-full w-full flex-col ">
-      <div className="flex flex items-center justify-start ">
+      <div className="flex items-center justify-start ">
+        {typeof data?.user.avatar !== undefined && data?.user.avatar !== "" ? (
+          <img
+            src={data?.user.avatar}
+            alt="Player avatar"
+            className="my-2 ml-2 mr-4 h-28 w-28 border border-black"
+          />
+        ) : (
+          <UserIcon className="my-2 ml-2 mr-4 h-28 w-28 border border-black text-neutral-700" />
+        )}
         <img
-          src={data?.user.avatar}
-          alt="Player avatar"
-          className="my-2 ml-2 mr-4 h-28 w-28 "
+          src={RankIcon(data?.user.rank)}
+          className="-translate-x-8 -translate-y-8"
         />
         <div className="my-4 flex flex-col text-left">
           <div className="text-xl font-bold">{data?.user.name}</div>
-          <div>Rank : {data?.user.rank}</div>
+
           <div>Statistics : TODO </div>
         </div>
       </div>
@@ -120,7 +156,13 @@ const DisplayUserProfile = ({
           MATCH HISTORY
         </div>
         {data.user.games.length === 0 ? (
-          <div> NO MATCH YET : TO DO - ICON + text </div>
+          <div className="flex flex-col">
+            <div className="mt-20 text-center text-2xl text-slate-300">
+              {" "}
+              {data.user.name} didn't play yet !
+            </div>
+            <PlayIcon className="text-slate-100" />
+          </div>
         ) : (
           <></>
         )}
@@ -138,7 +180,7 @@ const DisplayUserProfile = ({
               } mt-px flex h-12 items-center px-2`}
             >
               <img
-                className="h-10 w-10 object-cover "
+                className="h-10 w-10 border border-black object-cover"
                 src={game.player1.avatar}
                 alt="Player 1 avatar"
               />
@@ -150,7 +192,7 @@ const DisplayUserProfile = ({
                 {game.player2.name}
               </div>
               <img
-                className="h-10 w-10 object-cover"
+                className="h-10 w-10 border border-black object-cover"
                 src={game.player2.avatar}
                 alt="Player 2 avatar"
               />
@@ -215,6 +257,6 @@ export default function Profile() {
   return myData?.user.friends.some((friend) => friend.id == userId) ? (
     <DisplayUserProfile data={data} userId={userId} />
   ) : (
-    <Friend data={data} userId={userId} />
+    <DisplayAddAsFriend data={data} userId={userId} />
   );
 }
