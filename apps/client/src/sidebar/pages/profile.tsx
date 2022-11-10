a91c01b7cff26d62f9194302d12054dd6111215b
Jenny Eulliot <jeulliot@k1r2p1.42mulhouse.fr>
2022-11-10 16:06:54 +0100

    profile

--- apps/client/src/sidebar/pages/profile.tsx
+++ apps/client/src/sidebar/pages/profile.tsx
@@ -18,6 +18,9 @@ import Rank2Icon from "/src/assets/images/Rank2.svg";
 import Rank3Icon from "/src/assets/images/Rank3.svg";
 import Rank4Icon from "/src/assets/images/Rank4.svg";
 import Rank5Icon from "/src/assets/images/Rank5.svg";
+import ClassicIcon from "/src/assets/images/ClassicIcon.svg";
+import BonusIcon from "/src/assets/images/BonusIcon.svg";
+import FireIcon from "/src/assets/images/FireIcon.svg";
 import { ReactComponent as UserIcon } from "pixelarticons/svg/user.svg";
 import { ReactComponent as AddFriendIcon } from "pixelarticons/svg/user.svg";
 import { ReactComponent as PlayIcon } from "pixelarticons/svg/gamepad.svg";
@@ -96,7 +99,7 @@ const DisplayAddAsFriend = ({
         }}
         className="my-8 flex h-24 w-24 items-center justify-center self-center border-2 border-slate-300 bg-slate-200 p-2 text-center text-xl  font-bold transition-all  hover:cursor-pointer hover:bg-slate-300"
       >
-        Add as Friend
+        Add Friend
       </span>
       <AddFriendIcon className="w-96 self-center text-slate-100 " />
     </div>
@@ -127,9 +130,22 @@ const DisplayUserProfile = ({
       queryClient.invalidateQueries([]);
     },
   });
+  const numberOfGames = data?.user.games.length;
+  const victories = data?.user.games.filter((game) => {
+    if (
+      (game.player1.id === data?.user.id &&
+        game.player1score > game.player2score) ||
+      (game.player2?.id === data?.user.id &&
+        game.player2score > game.player1score)
+    )
+      return true;
+    else return false;
+  }).length;
+  const victoryRate = Math.floor((100 * victories) / numberOfGames);
 
   return (
     <div className="flex h-full w-full flex-col ">
+      {/* MAKE A COMPONENT OF THIS */}
       <div className="flex items-center justify-start ">
         {typeof data?.user.avatar !== undefined && data?.user.avatar !== "" ? (
           <img
@@ -144,14 +160,19 @@ const DisplayUserProfile = ({
           src={RankIcon(data?.user.rank)}
           className="-translate-x-8 -translate-y-8"
         />
-        <div className="my-4 flex flex-col text-left">
+        <div className="my-4 flex grow flex-col text-left">
           <div className="text-xl font-bold">{data?.user.name}</div>
-
-          <div>Statistics : TODO </div>
+          <div>Matchs played : {numberOfGames} </div>
+          <div>Victories : {victories} </div>
+          <div>Victory rate : {victoryRate} %</div>
+        </div>
+        <div className="mr-2 flex w-20 justify-end ">
+          PUT HERE THE ACHIEVEMENTS
         </div>
       </div>
 
-      <div className="flex w-full grow flex-col overflow-auto text-sm">
+      {/* MAKE A COMPONENT OF THIS */}
+      <div className="flex w-full grow flex-col overflow-auto p-1 text-sm">
         <div className="mt-8 pb-2 text-center text-xl font-bold">
           MATCH HISTORY
         </div>
@@ -170,44 +191,63 @@ const DisplayUserProfile = ({
           const victory =
             (game.player1.id === data?.user.id &&
               game.player1score > game.player2score) ||
-            (game.player2.id === data?.user.id &&
+            (game.player2?.id === data?.user.id &&
               game.player2score > game.player1score);
           return (
             <div
               key={index}
-              className={`${
-                victory ? "bg-red-300" : "bg-red-400 "
-              } mt-px flex h-12 items-center px-2`}
+              className="mt-1 flex h-12 w-full
+               items-center border border-slate-700 bg-slate-200 "
             >
-              <img
-                className="h-10 w-10 border border-black object-cover"
-                src={game.player1.avatar}
-                alt="Player 1 avatar"
-              />
-              <div className="ml-2 w-28 text-clip text-left">
-                {game.player1.name}
+              <div className="flex w-full ">
+                <img
+                  className="ml-1 h-10 w-10 border border-black object-cover "
+                  src={game.player1.avatar}
+                  alt="Player 1 avatar"
+                />
+                <div className="text-ellipsistext-left ml-2 w-32 self-center">
+                  {game.player1.name}
+                </div>
+                <div className="grow select-none self-center text-center text-lg font-bold ">
+                  VS
+                </div>
+                <div className="mr-2 flex w-32 justify-end self-center text-ellipsis text-right">
+                  {game.player2?.name}
+                </div>
+                <img
+                  className="h-10 w-10 justify-end border border-black object-cover"
+                  src={game.player2?.avatar}
+                  alt="Player 2 avatar"
+                />
               </div>
-              <div className="text-lg font-bold">VS</div>
-              <div className=" mr-2 flex w-28 justify-end text-clip">
-                {game.player2.name}
-              </div>
-              <img
-                className="h-10 w-10 border border-black object-cover"
-                src={game.player2.avatar}
-                alt="Player 2 avatar"
-              />
-              <div className=" mx-2 w-20 border-x-2 border-white text-center font-bold">
+              <div
+                className={`${
+                  victory ? "bg-green-400" : "bg-red-400"
+                } mx-1 flex h-full basis-1/6 flex-col justify-center border-x border-black text-center font-bold`}
+              >
                 {victory ? <div>VICTORY</div> : <div>DEFEAT</div>}
-
                 <div>
                   {game.player1score} - {game.player2score}
                 </div>
               </div>
-              <div className="self-center">{game.gamemode}</div>
+              <div className="flex justify-center">
+                <img
+                  className="h-8 w-10 "
+                  src={
+                    game.gamemode === "Classic"
+                      ? ClassicIcon
+                      : game.gamemode === "Random"
+                      ? BonusIcon
+                      : FireIcon
+                  }
+                />
+              </div>
             </div>
           );
         })}
       </div>
+
+      {/* MAKE A COMPONENT OF THIS */}
       <div className="flex h-24 bg-slate-100 text-xl font-bold">
         <div
           onClick={() => {
