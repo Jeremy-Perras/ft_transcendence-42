e17197d22bf5ffe86727422f69361e7c3c396acf
Jenny Eulliot <jeulliot@k1r2p1.42mulhouse.fr>
2022-11-11 17:37:42 +0100

    profile

--- apps/client/src/sidebar/pages/uploadAvatar.tsx
+++ apps/client/src/sidebar/pages/uploadAvatar.tsx
@@ -4,7 +4,13 @@ import { useUserProfileQuery } from "../../graphql/generated";
 import queryClient from "../../query";
 
 import { ReactComponent as AddAvatarIcon } from "pixelarticons/svg/cloud-upload.svg";
-export default function FileUploadPage() {
+export default function FileUploadPage({
+  open,
+  setIsOpen,
+}: {
+  open: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   const [selectedFile, setSelectedFile] = useState();
 
   useEffect(() => {
@@ -16,7 +22,10 @@ export default function FileUploadPage() {
     setSelectedFile(event);
   };
   return (
-    <div className="flex flex-row">
+    <div className="absolute z-10 flex h-40 flex-col items-center justify-center  border-2 bg-white">
+      <div className="flex border-2" onClick={() => setIsOpen(false)}>
+        Close
+      </div>
       <input
         type="file"
         onChange={(e) => {
@@ -25,23 +34,26 @@ export default function FileUploadPage() {
           }
         }}
       />
-      <button
-        onClick={() => {
-          if (selectedFile != null) {
-            const formData = new FormData();
-            formData.append("file", selectedFile);
-            fetch("/file/upload", {
-              method: "POST",
-              body: formData,
-            }).then(() => {
-              queryClient.invalidateQueries(useUserProfileQuery.getKey());
-            });
-          }
-        }}
-        className="flex border-2 border-black bg-slate-100 hover:bg-slate-200"
-      >
-        Submit
-      </button>
+      <div className="flex">
+        <button
+          onClick={() => {
+            if (selectedFile != null) {
+              const formData = new FormData();
+              formData.append("file", selectedFile);
+              fetch("/file/upload", {
+                method: "POST",
+                body: formData,
+              }).then(() => {
+                queryClient.invalidateQueries(useUserProfileQuery.getKey());
+              });
+            }
+            setIsOpen(false);
+          }}
+          className=" flex h-fit border-2 border-black bg-slate-100 hover:bg-slate-200"
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
