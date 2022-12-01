d820e2535a57026f906144e0599ac0e4af8f6ce5
Jeremy Perras <jperras@k2r4p6.42mulhouse.fr>
2022-12-01 09:32:59 +0100

    Service

--- server/src/socket/socket.gateway.ts
+++ server/src/socket/socket.gateway.ts
@@ -1,13 +1,17 @@
+import { SocketAction } from "@apps/shared";
 import {
+  MessageBody,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from "@nestjs/websockets";
 import { sortFns } from "@tanstack/react-query-devtools/build/lib/utils";
 import { Server, Socket } from "socket.io";
+import { SocketService } from "./socket.service";
 
 @WebSocketGateway({ cors: "*" })
 export class SocketGateway {
+  constructor(private socketService: SocketService) {}
   @WebSocketServer()
   server: Server;
 
@@ -17,4 +21,7 @@ export class SocketGateway {
       .to(client.request.session.passport.user.toString())
       .emit("test");
   }
+
+  @SubscribeMessage(SocketAction.DIRECT_MESSAGE)
+  async onNewDirectMessage(@MessageBody() body: string) {}
 }
--- shared/src/index.ts
+++ shared/src/index.ts
@@ -1,5 +1,7 @@
 enum SocketAction {
-  TEST,
+  DIRECT_MESSAGE,
+  CHANNEL_MESSAGE,
+  GAME_INVITATION,
 }
 
 export { SocketAction };
