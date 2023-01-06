8afe4c888e1edb152680c26a25a90d5979adb0ce
Jenny Eulliot <jenny.eulliot@gmail.com>
2023-01-06 14:08:02 +0000

    save

--- client/src/game/components/gameInvitation.tsx
+++ client/src/game/components/gameInvitation.tsx
@@ -4,27 +4,9 @@ import { useState } from "react";
 import { ReactComponent as AcceptIcon } from "pixelarticons/svg/check.svg";
 import { ReactComponent as RefuseIcon } from "pixelarticons/svg/close.svg";
 import { useNavigate } from "react-router-dom";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import request from "graphql-request";
 import { io } from "socket.io-client";
-
-import { useMediaQuery } from "@react-hookz/web";
-
-import { GameInvitationQuery } from "client/src/gql/graphql";
-
-// const GameInvitationQueryDocument = graphql(`
-//   query GameInvitation($gameId: Int!) {
-//     game(id: $gameId) {
-//       gameMode
-//       players {
-//         player1 {
-//           id
-//           name
-//         }
-//       }
-//     }
-//   }
-// `);
 
 const LaunchGameMutationDocument = graphql(`
   mutation LaunchGame($gameId: Int!) {
@@ -38,21 +20,9 @@ const RefuseGameInvitationMutationDocument = graphql(`
   }
 `);
 
-const Invitation = () => {
+const Invitation = ({ invitation }: { invitation: GameInvitation }) => {
   const navigate = useNavigate();
   const [display, setDisplay] = useState(true);
-
-  //   const launchGame = useMutation(
-  //     async ({ gameId }: { gameId: number }) =>
-  //       request("/graphql", LaunchGameMutationDocument, {
-  //         gameId: gameId,
-  //       }),
-  //     {
-  //       onSuccess: () => {
-  //         navigate(`/game/${gameId}`);
-  //       },
-  //     }
-  //   );
 
   const launchGame = useMutation(
     async ({ gameId }: { gameId: number }) =>
@@ -61,7 +31,7 @@ const Invitation = () => {
       }),
     {
       onSuccess: () => {
-        // navigate(`/game/${gameId?.game.}`);
+        navigate(`/game/${invitation.gameId}`);
       },
     }
   );
@@ -89,27 +59,26 @@ const Invitation = () => {
         <span className="mx-4 flex max-w-[80%] shrink grow-0 items-center whitespace-nowrap text-base tracking-wide">
           <img
             className="mx-2 h-8 w-8 shrink-0 border border-black"
-            src={`http://localhost:5173/upload/avatar/1`}
+            src={`http://localhost:5173/upload/avatar/${invitation.userIds[0]}`}
           />
-          {/* put userId */}
           <span
             className={`shrink grow-0 truncate`}
-          >{`Begrthiojrohijtroihtjrtiohjrthiojrthjreb `}</span>
-          <span className="w-content">{` invites you to play classic mode. Accept?`}</span>
+          >{`${invitation.userName}`}</span>
+          <span className="w-content">{` invites you to play ${invitation.gameMode} mode. Accept?`}</span>
         </span>
-        {/* <span>{`${gameInvitation.players.player1.name} invites you to play ${gameInvitation.gameMode} mode. Accept?`}</span> */}
+
         <div className="flex basis-1/5 justify-end ">
           <AcceptIcon
             className="h-8 w-8 animate-none border border-slate-300 bg-slate-200 hover:cursor-pointer hover:bg-slate-300"
             onClick={() => {
-              // launchGame.mutate({ gameId: gameInvitation.id });
+              launchGame.mutate({ gameId: invitation.gameId });
               setDisplay(false);
             }}
           />
           <RefuseIcon
             className="mx-2 h-8 w-8 border border-slate-300 bg-slate-200 hover:cursor-pointer hover:bg-slate-300"
             onClick={() => {
-              // refuseGameInvitation.mutate({ gameId: gameInvitation.id });
+              refuseGameInvitation.mutate({ gameId: invitation.gameId });
               setDisplay(false);
               // send refuse invitation notification from back
             }}
@@ -128,9 +97,8 @@ type GameInvitation = {
 };
 
 export const GameInvitations = () => {
-  let invitationList: GameInvitation[] = [
-    { gameId: 1, userIds: [1, 2], userName: "Bloublou", gameMode: "Classic" },
-  ];
+  const firstInvitation: GameInvitation[] = [];
+  const [invitationList, setInvitationList] = useState(firstInvitation);
 
   const socket = io();
 
@@ -141,13 +109,13 @@ export const GameInvitations = () => {
       gameId: targetId.gameId,
       userName: targetId.userName,
     };
-    invitationList = [newInvitation, ...invitationList];
+    setInvitationList([newInvitation, ...invitationList]);
   });
 
   return (
     <>
-      {invitationList.map((invite, index) => (
-        <Invitation key={index} />
+      {invitationList.map((invitation, index) => (
+        <Invitation key={index} invitation={invitation} />
       ))}
     </>
   );
