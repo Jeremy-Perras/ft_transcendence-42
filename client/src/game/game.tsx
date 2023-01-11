49d55306da80e83627a810fbbfdbcb79ec917dd4
Jeremy Perras <jeremy.perras@outlook.fr>
2023-01-11 09:00:17 +0000

    start Game

--- client/src/game/game.tsx
+++ client/src/game/game.tsx
@@ -1,3 +1,22 @@
-export const Game = () => {
-  return <div>game</div>;
+// import { useEffect, useRef } from "react";
+import React, { useRef, useEffect } from "react";
+export const Game = (props) => {
+  const canvasRef = useRef(null);
+
+  const draw = (ctx) => {
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    //Our draw come here
+    draw(context);
+  }, [draw]);
+
+  return <canvas ref={canvasRef} {...props} />;
 };
