8b0c7a1adfbb5262170a79f64f3d7dbc15927df7
Sebastien Waegeneire <sebastien@waegeneire.com>
2023-01-16 12:51:03 +0000

    wip

--- server/src/game/game.service.ts
+++ server/src/game/game.service.ts
@@ -13,10 +13,9 @@ export class GameService {
     private readonly socketGateway: SocketGateway
   ) {}
 
-  // TODO: private
-  players: Map<number, ReturnType<typeof PlayerMachine>> = new Map();
-  games: Map<number, Game> = new Map();
-  matchmakingRooms: Record<GameMode, Set<number>> = {
+  private players: Map<number, ReturnType<typeof PlayerMachine>> = new Map();
+  private games: Map<number, Game> = new Map();
+  private matchmakingRooms: Record<GameMode, Set<number>> = {
     CLASSIC: new Set(),
     RANDOM: new Set(),
     SPEED: new Set(),
@@ -135,6 +134,22 @@ export class GameService {
     }
   }
 
+  getMatchmakingRoom = (gameMode: GameMode) => this.matchmakingRooms[gameMode];
+
+  isInMatchmaking = (userId: number) => {
+    for (const mode in this.matchmakingRooms) {
+      if (this.matchmakingRooms[mode as GameMode].has(userId))
+        return mode as GameMode;
+    }
+    return null;
+  };
+
+  leaveMatchmaking = (userId: number) => {
+    for (const mode in this.matchmakingRooms) {
+      this.matchmakingRooms[mode as GameMode].delete(userId);
+    }
+  };
+
   getPlayer = (userId: number) => {
     let player = this.players.get(userId);
     if (player) return player;
@@ -162,44 +177,81 @@ export class GameService {
     }
   };
 
-  createGame = (gameMode: GameMode, player1Id: number, player2Id: number) => {
-    return this.prismaService.game.create({
-      data: {
-        mode: gameMode,
-        player1Id,
-        player2Id,
-      },
+  createGame = (gameMode: GameMode, player1Id: number, player2Id: number) =>
+    new Promise<void>((resolve, reject) => {
+      this.prismaService.game
+        .create({
+          data: {
+            mode: gameMode,
+            player1Id,
+            player2Id,
+          },
+        })
+        .then((game) => {
+          this.games.set(game.id, game);
+          resolve();
+        })
+        .catch(() => {
+          reject();
+        });
     });
-    // TODO: create timer callback
-  };
 
-  joinMatchmakingRoom = async (userId: number, gameMode: GameMode) => {
-    const matchmakingRoom = this.matchmakingRooms[gameMode];
-    if (matchmakingRoom.has(userId)) return;
-    console.log(1);
-    if (matchmakingRoom.size === 1) {
-      console.log(2);
-      const player1 = this.getPlayer(matchmakingRoom.values().next().value);
-      const player2 = this.getPlayer(userId);
-      if (player1 && player2) {
-        console.log(3);
-        const p1 = player1.getSnapshot();
-        const p2 = player2.getSnapshot();
-        console.log(p1.can("GAME_FOUND"));
-        if (p1.can("GAME_FOUND") && p2.can("GAME_FOUND")) {
-          console.log(4);
-          await this.createGame(gameMode, p1.context.userId, p2.context.userId);
-          player1.send({ type: "GAME_FOUND" });
-          player2.send({ type: "GAME_FOUND" });
-          return;
-        }
-      }
+  endGame = async (gameId: number) => {
+    const game = this.games.get(gameId);
+
+    if (game) {
+      await this.prismaService.game.update({
+        where: { id: gameId },
+        data: {
+          finishedAt: new Date(),
+          player1Score: 0, // TODO
+          player2Score: 0, // TODO
+        },
+      });
+
+      const p1 = this.getPlayer(game.player1Id);
+      const p2 = this.getPlayer(game.player2Id);
+
+      if (p1) p1.send({ type: "GAME_ENDED" });
+      if (p2) p2.send({ type: "GAME_ENDED" });
+
+      this.games.delete(gameId);
     }
-    matchmakingRoom.add(userId);
   };
 
-  leaveMatchmakingRoom = (userId: number, gameMode: GameMode) => {
-    const matchmakingRoom = this.matchmakingRooms[gameMode];
-    matchmakingRoom.delete(userId);
+  forfeitGame = async (userId: number) => {
+    const game = this.getGame(userId);
+
+    if (game) {
+      const data = {
+        finishedAt: new Date(),
+        player1Score: 0,
+        player2Score: 0,
+      };
+
+      switch (userId) {
+        case game.player1Id:
+          data.player1Score = -42;
+          data.player2Score = 11;
+          break;
+        default:
+          data.player1Score = 11;
+          data.player2Score = -42;
+          break;
+      }
+
+      await this.prismaService.game.update({
+        where: { id: game.id },
+        data,
+      });
+
+      const p1 = this.getPlayer(game.player1Id);
+      const p2 = this.getPlayer(game.player2Id);
+
+      if (p1) p1.send({ type: "GAME_ENDED" });
+      if (p2) p2.send({ type: "GAME_ENDED" });
+
+      this.games.delete(game.id);
+    }
   };
 }
