699d8b02fcdc49eb504e6dfadbf65b06aa0b0504
Jeremy Perras <jeremy.perras@outlook.fr>
2023-01-20 12:26:47 +0000

    Change front logo connection

--- client/src/sidebar/components/isOnline.tsx
+++ client/src/sidebar/components/isOnline.tsx
@@ -2,14 +2,12 @@ import { UserStatus } from "../../../src/gql/graphql";
 
 export const IsOnline = ({ userStatus }: { userStatus: UserStatus }) => {
   return userStatus === UserStatus.Online ? (
-    <span className="absolute top-0 left-0 flex h-1 w-1">
+    <span className="absolute top-0 left-0 flex h-2 w-2">
       <span className="absolute inline-flex h-full w-full bg-green-400 opacity-75"></span>
-      <span className="relative inline-flex h-1 w-1 bg-green-500"></span>
     </span>
   ) : (
-    <span className={`absolute top-0 left-0 flex h-1 w-1`}>
-      <span className="absolute inline-flex h-full w-full bg-red-400 opacity-75"></span>
-      <span className="relative inline-flex h-1 w-1 bg-red-500"></span>
+    <span className={`absolute top-0 left-0 flex h-2 w-2`}>
+      <span className="absolute inline-flex h-full w-full bg-red-300 opacity-75"></span>
     </span>
   );
 };
