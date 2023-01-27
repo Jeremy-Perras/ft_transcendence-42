97bcf3de373ebcec00903271845dc9cb386db67b
Jenny Eulliot <jenny.eulliot@gmail.com>
2023-01-27 14:02:02 +0000

    save

--- server/src/game/game.module.ts
+++ server/src/game/game.module.ts
@@ -2,6 +2,7 @@ import { forwardRef, Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
 import { PrismaModule } from "../prisma/prisma.module";
 import { SocketModule } from "../socket/socket.module";
+import { UserModule } from "../user/user.module";
 import { GameResolver } from "./game.resolver";
 import { GameService } from "./game.service";
 
