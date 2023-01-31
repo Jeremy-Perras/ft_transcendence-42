fd8c61f5815e0de97b0a1037f39ab8937f1ee97d
Jeremy Perras <jeremy.perras@outlook.fr>
2023-01-31 10:32:10 +0000

    Fix docker compose dev and player in game

--- client/src/sidebar/components/isOnline.tsx
+++ client/src/sidebar/components/isOnline.tsx
@@ -5,9 +5,13 @@ export const IsOnline = ({ userStatus }: { userStatus: UserStatus }) => {
     <span className="absolute top-0 left-0 flex h-2 w-2">
       <span className="absolute inline-flex h-full w-full  bg-green-500"></span>
     </span>
-  ) : (
+  ) : UserStatus.Offline ? (
     <span className={`absolute top-0 left-0 flex h-2 w-2`}>
       <span className="absolute inline-flex h-full w-full bg-red-500 "></span>
+    </span>
+  ) : (
+    <span className={`absolute top-0 left-0 flex h-2 w-2`}>
+      <span className="absolute inline-flex h-full w-full bg-orange-500 "></span>
     </span>
   );
 };
